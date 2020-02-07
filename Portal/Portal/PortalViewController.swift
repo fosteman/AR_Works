05e45611b1fc4a81f1acbc0824f6b0d1113a244e
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-02-07 14:46:33 -0500

    gracefully tackling session interruptions

--- Portal/Portal/PortalViewController.swift
+++ Portal/Portal/PortalViewController.swift
@@ -6,6 +6,8 @@ class PortalViewController: UIViewController {
   @IBOutlet var sceneView: ARSCNView?
   @IBOutlet weak var messageLabel: UILabel?
   @IBOutlet weak var sessionStateLabel: UILabel?
+  var debugPlanes: [SCNNode] = []
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -21,11 +23,34 @@ class PortalViewController: UIViewController {
     sessionStateLabel?.text = ""
   }
   
+  func showMessage(_ message: String, label: UILabel, seconds: Double) {
+    label.text = message
+    label.alpha = 1
+    DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+      if label.text == message {
+        label.text = ""
+        label.alpha = 0
+      }
+    }
+  }
+  
+  func removeAllNodes() {
+    removeDebugPlanes()
+  }
+  
+  func removeDebugPlanes() {
+    for n in self.debugPlanes {
+      n.removeFromParentNode()
+    }
+    self.debugPlanes = []
+  }
+  
   func runSession() {
     let config = ARWorldTrackingConfiguration()
     config.planeDetection = .horizontal
     config.isLightEstimationEnabled = true
-    sceneView?.session.run(config)
+    sceneView?.session.run(config, options: [.resetTracking,
+    .removeExistingAnchors]) ///session does not continue device position and motion tracking from previous configuration. And any objects associated with the session in its previius configuration are removed
     sceneView?.delegate = self
     
     #if DEBUG
@@ -43,6 +68,7 @@ extension PortalViewController: ARSCNViewDelegate {
         #if DEBUG
         let debugPlaneNode = createPlaneNode(center: planeAnchor.center, extent: planeAnchor.extent)
         node.addChildNode(debugPlaneNode)
+        self.debugPlanes.append(debugPlaneNode)
         #endif
         self.messageLabel?.text = "Tap on the detected horizontal plane to place te portal"
       }
@@ -58,7 +84,29 @@ extension PortalViewController: ARSCNViewDelegate {
                         extent: planeAnchor.extent)
       }
     }
+  }
+  
+  func sessionWasInterrupted(_ session: ARSession) {
+    guard let label = self.sessionStateLabel else {return}
     
+    showMessage("Session interrupted", label: label, seconds: 3)
+  }
+  
+  func sessionInterruptionEnded(_ session: ARSession) {
+    guard let label = self.sessionStateLabel else {return}
     
+    showMessage("Session resumed!", label: label, seconds: 3)
+    
+    DispatchQueue.main.async {
+      self.removeAllNodes()
+      self.resetLabels()
+    }
+    
+    runSession()
+  }
+  
+  func session(_ session: ARSession, didFailWithError error: Error) {
+    guard let label = self.sessionStateLabel else {return}
+    showMessage(error.localizedDescription, label: label, seconds: 3)
   }
 }
