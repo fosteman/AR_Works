1e94309eac1c6a8e2de44e2c472e261c0cf120b5
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-02-08 09:12:23 -0500

    re-organized

--- Portal/Portal/PortalViewController.swift
+++ Portal/Portal/PortalViewController.swift
@@ -3,36 +3,51 @@ import ARKit
 
 class PortalViewController: UIViewController {
 
+  @IBOutlet weak var crosshair: UIView!
   @IBOutlet var sceneView: ARSCNView?
-    @IBOutlet weak var crosshair: UIView!
-    @IBOutlet weak var messageLabel: UILabel?
+  @IBOutlet weak var messageLabel: UILabel?
   @IBOutlet weak var sessionStateLabel: UILabel?
-  var debugPlanes: [SCNNode] = []
-  var viewCenter: CGPoint {
-    return CGPoint(x: view.bounds.width / 2.0 , y: view.bounds.height / 2.0)
-  }
+  
   var portalNode: SCNNode? = nil
   var isPortalPlaced = false
-  
+  var debugPlanes: [SCNNode] = []
+  var viewCenter: CGPoint {
+    let viewBounds = view.bounds
+    return CGPoint(x: viewBounds.width / 2.0, y: viewBounds.height / 2.0)
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     resetLabels()
     runSession()
   }
+
+  func runSession() {
+    let configuration = ARWorldTrackingConfiguration.init()
+    configuration.planeDetection = .horizontal
+    configuration.isLightEstimationEnabled = true
+
+    sceneView?.session.run(configuration,
+                           options: [.resetTracking, .removeExistingAnchors])
+
+    #if DEBUG
+      sceneView?.debugOptions = [SCNDebugOptions.showFeaturePoints]
+    #endif
+
+    sceneView?.delegate = self
+  }
   
-  // MARK: Helpers
   func resetLabels() {
-    messageLabel?.alpha = 1
-    messageLabel?.text = "Move your phone around and allow the app tp find a plane."
-    
-    sessionStateLabel?.alpha = 1
+    messageLabel?.alpha = 1.0
+    messageLabel?.text = "Move the phone around and allow the app to find a plane. You will see a yellow horizontal plane."
+    sessionStateLabel?.alpha = 0.0
     sessionStateLabel?.text = ""
   }
   
   func showMessage(_ message: String, label: UILabel, seconds: Double) {
     label.text = message
     label.alpha = 1
+    
     DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
       if label.text == message {
         label.text = ""
@@ -48,69 +63,72 @@ class PortalViewController: UIViewController {
   }
   
   func removeDebugPlanes() {
-    for n in self.debugPlanes {
-      n.removeFromParentNode()
+    for debugPlaneNode in self.debugPlanes {
+      debugPlaneNode.removeFromParentNode()
     }
     self.debugPlanes = []
   }
   
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    if let hit = sceneView?.hitTest(viewCenter, types: [.existingPlaneUsingExtent]).first {
+      sceneView?.session.add(anchor: ARAnchor.init(transform: hit.worldTransform))
+    }
+  }
+  
   func makePortal() -> SCNNode {
     let portal = SCNNode()
-    let box = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0)
+    let box = SCNBox(width: 1.0,
+                     height: 1.0,
+                     length: 1.0,
+                     chamferRadius: 0)
     let boxNode = SCNNode(geometry: box)
     portal.addChildNode(boxNode)
     return portal
   }
   
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    if let hit = sceneView?.hitTest(viewCenter, types: [.existingPlaneUsingExtent]).first {
-      sceneView?.session.add(anchor: ARAnchor(transform: hit.worldTransform)) ///
-    }
-  }
 }
 
 extension PortalViewController: ARSCNViewDelegate {
   
-  // MARK: SceneKit Mgmnt
-  /// Is called when ARSession detects new plane, and the ARSCNView automatically adds an ARPlaneAnchor for the plane, and when user taps on screen having at least one detected plane rendered
   func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
     DispatchQueue.main.async {
-      if let planeAnchor = anchor as? ARPlaneAnchor,
-        !self.isPortalPlaced {
+      if let planeAnchor = anchor as? ARPlaneAnchor, !self.isPortalPlaced {
         #if DEBUG
-        let debugPlaneNode = createPlaneNode(center: planeAnchor.center, extent: planeAnchor.extent)
-        node.addChildNode(debugPlaneNode)
-        self.debugPlanes.append(debugPlaneNode)
+          let debugPlaneNode = createPlaneNode(
+            center: planeAnchor.center,
+            extent: planeAnchor.extent)
+          node.addChildNode(debugPlaneNode)
+          self.debugPlanes.append(debugPlaneNode)
         #endif
         self.messageLabel?.alpha = 1.0
-        self.messageLabel?.text = "Tap on the detected horizontal plane to place te portal"
+        self.messageLabel?.text = "Tap on the detected horizontal plane to place the portal"
       }
       else if !self.isPortalPlaced {
+        
         self.portalNode = self.makePortal()
         if let portal = self.portalNode {
-          //place node at the location
           node.addChildNode(portal)
           self.isPortalPlaced = true
           
-          //reset debug planes
           self.removeDebugPlanes()
           self.sceneView?.debugOptions = []
           
-          //Update messageLabel
           DispatchQueue.main.async {
             self.messageLabel?.text = ""
             self.messageLabel?.alpha = 0
           }
         }
+        
       }
     }
   }
   
-  
-  func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+  func renderer(_ renderer: SCNSceneRenderer,
+                didUpdate node: SCNNode,
+                for anchor: ARAnchor) {
     DispatchQueue.main.async {
       if let planeAnchor = anchor as? ARPlaneAnchor,
-      node.childNodes.count > 0,
+        node.childNodes.count > 0,
         !self.isPortalPlaced {
         updatePlaneNode(node.childNodes[0],
                         center: planeAnchor.center,
@@ -118,53 +136,38 @@ extension PortalViewController: ARSCNViewDelegate {
       }
     }
   }
-  func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+  
+  func renderer(_ renderer: SCNSceneRenderer,
+                updateAtTime time: TimeInterval) {
     DispatchQueue.main.async {
-      if let _ = self.sceneView?.hitTest(self.viewCenter, types: [.existingPlaneUsingExtent]).first {
+      if let _ = self.sceneView?.hitTest(self.viewCenter,
+                                         types: [.existingPlaneUsingExtent]).first {
         self.crosshair.backgroundColor = UIColor.green
-        }
-      else {
+      } else {
         self.crosshair.backgroundColor = UIColor.lightGray
       }
     }
   }
   
-  // MARK: ARKit Session Mgmnt
-  func runSession() {
-    let config = ARWorldTrackingConfiguration()
-    config.planeDetection = .horizontal
-    config.isLightEstimationEnabled = true
-    sceneView?.session.run(config, options: [.resetTracking,
-    .removeExistingAnchors]) ///session does not continue device position and motion tracking from previous configuration. And any objects associated with the session in its previius configuration are removed
-    sceneView?.delegate = self
-    
-    #if DEBUG
-    sceneView?.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
-    #endif
+  func session(_ session: ARSession, didFailWithError error: Error) {
+    guard let label = self.sessionStateLabel else { return }
+    showMessage(error.localizedDescription, label: label, seconds: 3)
   }
   
   func sessionWasInterrupted(_ session: ARSession) {
-    guard let label = self.sessionStateLabel else {return}
-    
+    guard let label = self.sessionStateLabel else { return }
     showMessage("Session interrupted", label: label, seconds: 3)
   }
-  
+
   func sessionInterruptionEnded(_ session: ARSession) {
-    guard let label = self.sessionStateLabel else {return}
-    
-    showMessage("Session resumed!", label: label, seconds: 3)
+    guard let label = self.sessionStateLabel else { return }
+    showMessage("Session resumed", label: label, seconds: 3)
     
     DispatchQueue.main.async {
       self.removeAllNodes()
       self.resetLabels()
     }
-    
     runSession()
   }
   
-  func session(_ session: ARSession, didFailWithError error: Error) {
-    guard let label = self.sessionStateLabel else {return}
-    showMessage(error.localizedDescription, label: label, seconds: 3)
-  }
 }
-
