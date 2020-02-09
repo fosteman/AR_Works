ab994d00e661dfc88eccf371d7b3eac7125c59a8
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-02-09 15:41:00 -0500

    implemented view controller to display a carousel

---
+++ KioskAd/KioskAd/ui/BillboardView.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+protocol BillboardViewDelegate: class {
+  func billboardViewDidSelectPlayVideo(_ view: BillboardView)
+}
+
+class BillboardView: UIView {
+  @IBOutlet weak var currentImageView: UIImageView!
+  @IBOutlet weak var previousButton: UIButton!
+  @IBOutlet weak var nextButton: UIButton!
+  
+  weak var delegate: BillboardViewDelegate?
+  
+  private var currentIndex: Int = 0
+  
+  func showPreviousImage() {
+    let currentX = currentImageView.frame.origin.x
+    let newIndex: Int = {
+      var index = (self.currentIndex - 1) % self.images.count
+      if index < 0 {
+        index = self.images.count + index
+      }
+      return index
+    }()
+
+    UIView.animate(withDuration: 0.2, animations: {
+      self.currentImageView.frame.origin.x += self.bounds.width
+    }, completion: { (finished) in
+      self.currentImageView.frame.origin.x = -self.currentImageView.frame.width
+      self.showImage(at: newIndex)
+      UIView.animate(withDuration: 0.5, animations: {
+        self.currentImageView.frame.origin.x = currentX
+      })
+    })
+  }
+  
+  func showNextImage() {
+    let currentX = currentImageView.frame.origin.x
+    let newIndex: Int = (currentIndex + 1) % images.count
+    
+    UIView.animate(withDuration: 0.2, animations: {
+      self.currentImageView.frame.origin.x -= self.bounds.width
+    }, completion: { (finished) in
+      self.currentImageView.frame.origin.x = self.currentImageView.frame.width + currentX
+      self.showImage(at: newIndex)
+      UIView.animate(withDuration: 0.5, animations: {
+        self.currentImageView.frame.origin.x = currentX
+      })
+    })
+  }
+  
+  private func showImage(at index: Int) {
+    currentIndex = index
+    currentImageView.image = images[currentIndex]
+  }
+  
+  var images: [UIImage]! {
+    didSet {
+      showImage(at: 0)
+    }
+  }
+  
+  @IBAction func didTapPrevious() {
+    showPreviousImage()
+  }
+  
+  @IBAction func didTapNext() {
+    showNextImage()
+  }
+  
+  @IBAction func didTapPlayVideo() {
+    self.delegate?.billboardViewDidSelectPlayVideo(self)
+  }
+}
+
