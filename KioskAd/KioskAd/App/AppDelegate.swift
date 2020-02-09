3a5ab355c51cc9b9e93aae6f1c7377bd51731a51
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-02-09 13:57:18 -0500

    cleanup

---
+++ KioskAd/KioskAd/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  KioskAd
+//
+//  Created by Tim Fosteman on 2020-02-09.
+//  Copyright © 2020 Fosteman's Software Solutions LLC. All rights reserved.
+//
+
+import UIKit
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
