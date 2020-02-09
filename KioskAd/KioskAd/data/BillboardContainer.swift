9dbeabe5c67a2321a7062922b9258160fa143ee3
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-02-09 13:07:52 -0500

    Groupping of ui, data, app elements

---
+++ KioskAd/KioskAd/data/BillboardContainer.swift
@@ -0,0 +1,19 @@
+//
+
+import ARKit
+import SceneKit
+
+struct BillboardContainer {
+  var billboardAnchor: ARAnchor
+  var billboardNode: SCNNode?
+  var plane: RectangularPlane
+
+  var hasBillboardNode: Bool { return billboardNode != nil }
+
+  init(billboardAnchor: ARAnchor, plane: RectangularPlane) {
+    self.billboardAnchor = billboardAnchor
+    self.plane = plane
+    self.billboardNode = nil
+  }
+}
+
---
+++ KioskAd/KioskAd/ui/AdViewController.swift
@@ -0,0 +1,66 @@
+import UIKit
+import SceneKit
+import ARKit
+
+class AdViewController: UIViewController {
+  @IBOutlet var sceneView: ARSCNView!
+  weak var targetView: TargetView!
+
+  private var billboard: BillboardContainer?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    sceneView.delegate = self
+    sceneView.session.delegate = self
+    sceneView.showsStatistics = true
+    
+    let scene = SCNScene()
+    sceneView.scene = scene
+
+    // Setup the target view
+    let targetView = TargetView(frame: view.bounds)
+    view.addSubview(targetView)
+    self.targetView = targetView
+    targetView.show()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    // Create a session configuration
+    let configuration = ARWorldTrackingConfiguration()
+
+    // Run the view's session
+    sceneView.session.run(configuration)
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+
+    // Pause the view's session
+    sceneView.session.pause()
+  }
+}
+
+// MARK: - ARSCNViewDelegate
+extension AdViewController: ARSCNViewDelegate {
+}
+
+extension AdViewController: ARSessionDelegate {
+  func session(_ session: ARSession, didFailWithError error: Error) {
+  }
+
+  func sessionWasInterrupted(_ session: ARSession) {
+  }
+
+  func sessionInterruptionEnded(_ session: ARSession) {
+  }
+}
+
+extension AdViewController {
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+  }
+}
+
+private extension AdViewController {
+}
